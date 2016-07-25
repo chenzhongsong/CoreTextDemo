9fcb567c4012a03942bc8363b202f455964a1be3
eliteall <chenzhongsong@eliteall.com>
2016-07-25 18:27:04 +0800

    第二次提交

--- CoreTextDemo/CoreTextDemo/MVC/View/View/CTDisplayView.m
+++ CoreTextDemo/CoreTextDemo/MVC/View/View/CTDisplayView.m
@@ -27,10 +27,11 @@
     
     //步骤3
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, self.bounds);
+//    CGPathAddRect(path, NULL, self.bounds);
+    CGPathAddEllipseInRect(path, NULL, self.bounds);
     
     //步骤4
-    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:@"Hello World"];
+    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:@"Hello World！创建绘制的区域，CoreText本身支持各种文字排版的区域，我们这里简单的将UIView的整个界面作为排版的区域，为了加深理解，建议读者将该步骤的代码替换成如下代码，测试不同的绘制区域带来的界面变化。"];
     CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
     CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, [attString length]), path, NULL);
     
